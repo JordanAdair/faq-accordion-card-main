bad6b2b5fe7bc5551573d75ad56040bf1433d02f
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-07-05 21:33:19 -0400

    Finished project

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     - Get each element with the class name of "question"
     - Loop through each element returned
     - Add a click event to each element
-    - Once clicked, get the next element sibling (the answer)
+    - Once clicked, close any open tabs and get the next element sibling (the answer)
     - Toggle the "active" state
     - If the answer is shown, hide. / If the answer is hidden, show.
 */
@@ -11,6 +11,14 @@ const questions = document.getElementsByClassName("question");
 
 for (i = 0; i < questions.length; i++) {
 	questions[i].addEventListener("click", function () {
+		// Close any already open tabs
+		for (let j = 0; j < questions.length; j++) {
+			if (questions[j].nextElementSibling.style.maxHeight) {
+				questions[j].nextElementSibling.style.maxHeight = null;
+				questions[j].classList.remove("active");
+			}
+		}
+
 		this.classList.toggle("active");
 		var answer = this.nextElementSibling;
 		if (answer.style.maxHeight) {
