f5d31fd3fcbcc63230f2475de614a8f54cbddfd5
Jordan <43575081+JordanAdair@users.noreply.github.com>
2022-07-05 22:42:01 -0400

    Added ability to close a drawer by clicking it instead of its neighbors.

--- main.js
+++ main.js
@@ -11,19 +11,18 @@ const questions = document.getElementsByClassName("question");
 
 for (i = 0; i < questions.length; i++) {
 	questions[i].addEventListener("click", function () {
-		// Close any already open tabs
-		for (let j = 0; j < questions.length; j++) {
-			if (questions[j].nextElementSibling.style.maxHeight) {
-				questions[j].nextElementSibling.style.maxHeight = null;
-				questions[j].classList.remove("active");
-			}
-		}
-
 		this.classList.toggle("active");
 		var answer = this.nextElementSibling;
 		if (answer.style.maxHeight) {
 			answer.style.maxHeight = null;
 		} else {
+			// Close any open tabs
+			for (let j = 0; j < questions.length; j++) {
+				if (questions[j].nextElementSibling.style.maxHeight) {
+					questions[j].nextElementSibling.style.maxHeight = null;
+					questions[j].classList.remove("active");
+				}
+			}
 			answer.style.maxHeight = answer.scrollHeight + "px";
 		}
 	});
